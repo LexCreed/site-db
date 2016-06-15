f270319175c6f115cece5cabdf59d8e48a34c710
Elias Abraham Hernandez Ramirez <naster0007@gmail.com>
2016-06-15 09:38:24 -0500

    meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,6 @@
 module ApplicationHelper
 
-    def article_cover url, options = {}
-    
-        html_class = options[:class]
-        html_style = "background:url(#{url});"\
-                   "width:auto;height:29em;padding:16em; background-size:contain;"             
-                   
-        html = "<header style='#{html_style} ' class='#{html_class}'>"\
-                "</header>"
-        html.html_safe
-    end
+ 
 	  
 	def resource_name
 		:user
